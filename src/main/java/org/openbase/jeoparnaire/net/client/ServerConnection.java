cf4699539a151b06ffdbe0aed10bbbe97f5ec722
Divine Threepwood <divine@openbase.org>
2019-04-17 18:25:31 +0200

    apply header update

--- src/main/java/org/openbase/jeoparnaire/net/client/ServerConnection.java
+++ src/main/java/org/openbase/jeoparnaire/net/client/ServerConnection.java
@@ -4,7 +4,7 @@ package org.openbase.jeoparnaire.net.client;
  * #%L
  * Jeoparnaire
  * %%
- * Copyright (C) 2011 - 2018 openbase.org
+ * Copyright (C) 2011 - 2019 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
