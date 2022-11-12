b279974abc5682f7c0b256f519690e433963d5ca
Divine Threepwood <divine@openbase.org>
2022-11-12 13:49:14 +0100

    setup maven wrapper and fix install scritp. Updated licence header.

--- src/main/java/org/openbase/jeoparnaire/view/GameStyle.java
+++ src/main/java/org/openbase/jeoparnaire/view/GameStyle.java
@@ -5,7 +5,7 @@ package org.openbase.jeoparnaire.view;
  * #%L
  * Jeoparnaire
  * %%
- * Copyright (C) 2011 - 2020 openbase.org
+ * Copyright (C) 2011 - 2022 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
--- src/main/java/org/openbase/jeoparnaire/view/server/QuestPanel.java
+++ src/main/java/org/openbase/jeoparnaire/view/server/QuestPanel.java
@@ -4,7 +4,7 @@ package org.openbase.jeoparnaire.view.server;
  * #%L
  * Jeoparnaire
  * %%
- * Copyright (C) 2011 - 2020 openbase.org
+ * Copyright (C) 2011 - 2022 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
